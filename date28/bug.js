7971c607e713ee5291764c7f73d45f35b7f9dd3a
Piyas Ahmed <apiyas761@gmail.com>
2026-07-28 12:14:12 +0600

    count positive numbers in an array

--- date28/bug.js
+++ date28/bug.js
@@ -377,3 +377,21 @@ console.log("--------------------");
   console.log(largestOdd([12, 5, 18, 21, 7]));
 }
 console.log("--------------------");
+{
+  // Bug Challenge 29
+  //fix: count positive numbers in an array
+  function countPositive(arr) {
+    let count = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > 0) {
+        count++;
+      }
+    }
+
+    return count;
+  }
+
+  console.log(countPositive([-1, 2, 5, -3, 9]));
+}
+console.log("--------------------");
