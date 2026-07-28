5769af302b4e6affe81de04ba478e1691d66c437
Piyas Ahmed <apiyas761@gmail.com>
2026-07-28 14:46:05 +0600

    analyze an array to calculate sum, average, maximum, and minimum values

--- date28/bug.js
+++ date28/bug.js
@@ -479,3 +479,34 @@ console.log("--------------------");
   console.log(firstNegative([5, 10, -2, -7]));
 }
 console.log("--------------------");
+{
+  //  Bug Challenge 35 (🔥 Boss)
+  // fix: analyze an array to calculate sum, average, maximum, and minimum values
+  function analyzeArray(arr) {
+    let sum = 0;
+    let max = arr[0];
+    let min = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+
+      if (arr[i] < min) {
+        min = arr[i];
+      }
+    }
+
+    return {
+      sum: sum,
+      average: sum / arr.length,
+      max: max,
+      min: min,
+    };
+  }
+
+  console.log(analyzeArray([12, 8, 20, 5, 15]));
+}
+console.log("--------------------");
