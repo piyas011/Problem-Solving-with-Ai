37e5d0a8b919afb4eabe817a39851bad4244e497
Piyas Ahmed <apiyas761@gmail.com>
2026-07-28 09:46:30 +0600

    Bug Fix

---
+++ date28/bug.js
@@ -0,0 +1,75 @@
+{
+  //Bug Challenge 9: Reverse String
+  function reverseString(str) {
+    let reverse = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+      reverse += str[i];
+    }
+
+    return reverse;
+  }
+
+  console.log(reverseString("JavaScript"));
+}
+console.log("--------------------");
+{
+  // Bug Challenge 10: Find Number
+  function hasNumber(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === target) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  console.log(hasNumber([5, 10, 15, 20], 20));
+}
+console.log("--------------------");
+{
+  //  Bug Challenge 11 //\\ Hard Level
+  function factorial(n) {
+    let result = 1;
+
+    for (let i = 1; i <= n; i++) {
+      result *= i;
+    }
+
+    return result;
+  }
+
+  console.log(factorial(5));
+}
+
+console.log("--------------------");
+{
+  // Bug Challenge 12
+  const numbers = [2, 4, 6, 8];
+  let result;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      result = "All numbers are even";
+    }
+  }
+  console.log(result);
+}
+console.log("--------------------");
+{
+  // Bug Challenge 13
+  function removeNegative(arr) {
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > 0) {
+        result.push(arr[i]);
+      }
+    }
+
+    return result;
+  }
+
+  console.log(removeNegative([-2, 5, -1, 8, 0, 9]));
+}
+console.log("--------------------");
